aee394fa742baa1e572299b6ea08fcf2e5b45181
Rishab Gupta <88284783+Rishab4125@users.noreply.github.com>
2026-04-03 00:39:07 +0530

    Update session headers for requests in main.py

--- backend/main.py
+++ backend/main.py
@@ -29,11 +29,16 @@ def _clean_float(val) -> Optional[float]:
 load_dotenv()
 
 session = requests.Session()
-session.headers.update({"User-Agent": "Mozilla/5.0 (compatible; Nifty50ReturnsBot/1.0)"})
+# session.headers.update({"User-Agent": "Mozilla/5.0 (compatible; Nifty50ReturnsBot/1.0)"})
 # session.headers.update({
 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
 # })
+session.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Accept": "text/html,application/xhtml+xml",
+        "Connection": "keep-alive"
+    })
 yf.utils.requests = lambda : session
 
 
